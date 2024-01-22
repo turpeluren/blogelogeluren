acfa00f67929e895f96d0e83c8e62d574b17f426
turpeluren <thenotteam@gmail.com>
2024-01-22 21:18:13 +0000

    testing commentbox.io

---
+++ src/components/Commentbox.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import commentBox from 'commentbox.io';
+
+export default class PageWithComments extends React.Component {
+
+    componentDidMount() {
+
+        this.removeCommentBox = commentBox('my-project-id');
+    }
+
+    componentWillUnmount() {
+
+        this.removeCommentBox();
+    }
+
+    render() {
+
+        return (
+            <div className="commentbox" />
+        );
+    }
+}
