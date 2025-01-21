6194a112745ab0096e7995495931dcc0f31f5a1e
turpeluren <thenotteam@gmail.com>
2025-01-21 16:30:19 +0100

    new webhook

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+	readonly DISCORD_WEBHOOK: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
